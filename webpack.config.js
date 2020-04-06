348771987361349e829a6ed323c48343f383f915
SheepTester <ussa.say@gmail.com>
2020-04-06 15:38:49 -0700

    Pack CSS as well, very epic

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const TerserJSPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
 const production = process.env.NODE_ENV === 'production'
 
 module.exports = {
@@ -6,6 +10,34 @@ module.exports = {
   devtool: production ? 'source-map' : 'inline-source-map',
   devServer: {
     contentBase: '.'
+  },
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin(),
+      new OptimizeCSSAssetsPlugin()
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.min.css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: true,
+              hmr: !production
+            }
+          },
+          'css-loader'
+        ]
+      }
+    ]
   },
   output: {
     filename: 'bundle.min.js',
